a962847dcb0703c9305af32c749bdf47f9327884
Z_runner <z.runner.mc@gmail.com>
2018-08-07 16:15:01 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, ZRunner'
 author = 'ZRunner'
 
 # The short X.Y version
-version = ''
+version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = ''
+release = '1.0.0a'
 
 
 # -- General configuration ---------------------------------------------------
@@ -62,7 +62,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'fr'
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
