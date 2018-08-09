8c20ae88a5942d65f6daa55b0446582825db52db
Z_runner <z.runner.mc@gmail.com>
2018-08-10 00:29:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,22 @@ from setuptools import setup
 
 setup(
     name="frmc_lib",
-    version = "1.0.0a",
+    version = "1.0.2a",
     description = 'A Python 3 library that allows you to retrieve Minecraft game information from the fr-minecraft.net website',
     author = 'ZRunner',
     author_email = 'z.runner.mc@gmail.com',
-    url = 'https://github.com/ZRunner/fr-mc-python-lib.git',
-    py_modules=['frmc_lib'],
+    url = 'https://github.com/ZRunner/fr-mc-python-lib',
+    packages=['frmc_lib'],
     install_requires=[
         'requests',
         'regex'],
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    )
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
 )
