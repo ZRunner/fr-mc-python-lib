757ddd4c06c9609a659475be811b03375eb1e505
Z_runner <z.runner.mc@gmail.com>
2018-08-10 00:38:55 +0200

    Update __init__.py

--- frmc_lib/__init__.py
+++ frmc_lib/__init__.py
@@ -1,1 +1,14 @@
-name = "frmc_lib"
+# -*- coding: utf-8 -*-
+
+"""
+Hey I'm a test
+"""
+
+
+__title__ = 'frmcLib'
+__author__ = 'ZRunner'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 ZRunner'
+__version__ = '1.0.2'
+
+from .frmc_lib import *
