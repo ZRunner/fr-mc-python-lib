845b833a531abe721ecf5dfd3b49090af44084f0
Z_runner <z.runner.mc@gmail.com>
2018-08-09 22:58:18 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'ZRunner'
 # The short X.Y version
 version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '1.0.1'
+release = '1.0.2'
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
